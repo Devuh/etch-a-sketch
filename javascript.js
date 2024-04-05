a2a3b20552a4b03fa38ae4f2cbb7c40796471595
Devuh <david.walter.reichert@gmail.com>
2024-04-05 16:48:52 -0500

    Add mouseover drawing effect to squares

--- javascript.js
+++ javascript.js
@@ -1,6 +1,4 @@
 function createGrid(size) {
-    let container = document.querySelector("#container");
-
     for(let i = 0; i < size; i++) {
         let row = document.createElement("div");
         row.classList.add("row");
@@ -8,11 +6,20 @@ function createGrid(size) {
         for(let i = 0; i < size; i++) {
             let square = document.createElement("div");
             square.classList.add("square");
+            onHover(square);
             row.appendChild(square);
         }
         
         container.appendChild(row);
     }
 }
+
+function onHover(square) {
+    square.addEventListener("mouseover", (event) => {
+        event.target.classList.add("square-hover");
+    });
+}
+
+let container = document.querySelector("#container");
 
 createGrid(16);
